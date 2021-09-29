246c5eebfb10cf66f23c0495a46541b9171b81e6
raphael gregg <raphaelgregg@gmail.com>
2021-09-29 00:23:10 -0300

    feat: create hook useTransaction

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import api from './services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import api from '../services/api';
 
 interface Transaction {
   id: number;
@@ -30,7 +30,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -62,3 +62,8 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   )
 }
 
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
+}
