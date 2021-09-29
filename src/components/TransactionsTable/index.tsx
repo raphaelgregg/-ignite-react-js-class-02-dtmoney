6640baae8c81b7ed33c1ac33b4f1ffb3f559077c
raphael gregg <raphaelgregg@gmail.com>
2021-09-29 00:01:03 -0300

    feat: finish insert transaction

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -22,6 +22,7 @@ export function TransactionsTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
+                {transaction.type === "withdraw" && '-'}
                 {new Intl.NumberFormat('pt-BR', {
                   style: 'currency',
                   currency: 'BRL'
